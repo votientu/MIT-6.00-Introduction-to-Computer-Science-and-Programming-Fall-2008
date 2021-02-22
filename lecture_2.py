fac01211f78e4daee1d084c473f323fda83e3d88
votientu <votientu@gmail.com>
2021-02-22 09:53:27 +0100

    initial commit

---
+++ lecture_2.py
@@ -0,0 +1,14 @@
+# sample code for use in Lecture 2, Fall 2008
+
+##x = 3   # Create variable x and assign value 3 to it
+##x = x*x # Bind x to value 9
+##print(x)
+##
+##n = input('Enter a number: ')
+##print(n)
+### print(n/n)
+
+x = 15
+if x%2 == 0:
+    print('Even')
+else: print('Odd')
